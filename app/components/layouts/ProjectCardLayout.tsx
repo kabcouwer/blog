a1ef52d41d412ef6d528cf13cd3c712cce34140e
Kim Abcouwer <kim@aktivate.com>
2023-04-12 19:36:22 -0600

    Add projects menu

--- app/components/layouts/ProjectCardLayout.tsx
+++ app/components/layouts/ProjectCardLayout.tsx
@@ -28,46 +28,40 @@ export default function ProjectCardLayout(props: ProjectCardProps) {
   const isHidden = id === "project_portfolio";
 
   return (
-    <div id={id}>
-      <div className="container mx-auto px-5 py-12 text-black dark:text-white cursor-auto">
-        <div className="md:w-4/5 mx-auto flex items-center justify-center">
-          <div className="md:py-6 mt-6 md:mt-0 cursor-auto">
-            <h1 className="text-3xl title-font font-medium mb-1 cursor-auto">
-              {title}
-            </h1>
-            <h2 className="text-sm title-font tracking-widest mb-1 cursor-auto">
-              {dates}
-            </h2>
-            <h2 className="text-sm title-font tracking-widest mb-1 cursor-auto">
-              {subtitle}
-            </h2>
-            <div className="md:flex mb-4">
-              <BadgeArray skills={skills} />
-            </div>
-            <div className="leading-relaxed max-w-[600px]">
-              {description}
-            </div>
-            <div className="flex mt-6 items-center pb-5 mb-5">
-              <div className="flex">
-                <Link href={source} target="_blank" rel="noopener noreferrer">
-                  <button className="transform mouse-pointer bg-gray-900 text-sm mr-2 py-1 px-2 font-semibold rounded hover:bg-gray-700 dark:hover:bg-gray-600  hover:scale-90 dark:focus:bg-gray-600 focus:outline-none">
-                    Code
-                  </button>
-                </Link>
-                <Link href={demo} target="_blank" rel="noopener noreferrer">
-                  <button
-                    className={
-                      isHidden
-                        ? "hidden"
-                        : "transform mouse-pointer bg-gray-900 text-sm mr-2 py-1 px-2 font-semibold rounded hover:bg-gray-700 dark:hover:bg-gray-600 hover:scale-90 focus:bg-gray-700 dark:focus:bg-gray-600 focus:outline-none"
-                    }
-                  >
-                    Live
-                  </button>
-                </Link>
-              </div>
-            </div>
-          </div>
+    <div id={id} className="md:pl-12 cursor-auto">
+      <h1 className="text-5xl title-font font-medium pb-1 cursor-auto">
+        {title}
+      </h1>
+      <h2 className="title-font tracking-widest pb-2 cursor-auto">
+        {dates}
+      </h2>
+      <h2 className="title-font tracking-widest pb-2 cursor-auto">
+        {subtitle}
+      </h2>
+      <div className="md:flex mb-4">
+        <BadgeArray skills={skills} />
+      </div>
+      <div className="leading-relaxed max-w-[600px]">
+        {description}
+      </div>
+      <div className="flex mt-6 items-center pb-5 mb-5">
+        <div className="flex">
+          <Link href={source} target="_blank" rel="noopener noreferrer">
+            <button className="transform mouse-pointer bg-gray-900 text-sm mr-2 py-1 px-2 font-semibold rounded hover:bg-gray-700 dark:hover:bg-gray-600  hover:scale-90 dark:focus:bg-gray-600 focus:outline-none">
+              Code
+            </button>
+          </Link>
+          <Link href={demo} target="_blank" rel="noopener noreferrer">
+            <button
+              className={
+                isHidden
+                  ? "hidden"
+                  : "transform mouse-pointer bg-gray-900 text-sm mr-2 py-1 px-2 font-semibold rounded hover:bg-gray-700 dark:hover:bg-gray-600 hover:scale-90 focus:bg-gray-700 dark:focus:bg-gray-600 focus:outline-none"
+              }
+            >
+              Live
+            </button>
+          </Link>
         </div>
       </div>
     </div>
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,8 +1,9 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 import PortfolioDescription from "../content/project-portfolio.mdx";
+import MarketMoneyDescription from "../content/market-money.mdx";
 
 import ProjectCardLayout from "../components/layouts/ProjectCardLayout";
 
@@ -43,6 +44,42 @@ const projectItems = [
     ],
     description: <PortfolioDescription />,
   },
+  {
+    id: "market_money",
+    title: "Market Money",
+    subtitle: "Rails REST API",
+    dates: "Apr 2023",
+    demo: "",
+    source: "https://github.com/kabcouwer/market_money",
+    skills: [
+      {
+        text: "Ruby on Rails",
+        color: "green-800",
+        bgColor: "green-100",
+      },
+      {
+        text: "RSpec",
+        color: "rose-800",
+        bgColor: "rose-100",
+      },
+      {
+        text: "Postman",
+        color: "sky-800",
+        bgColor: "sky-100",
+      },
+      {
+        text: "Faker",
+        color: "orange-800",
+        bgColor: "orange-100",
+      },
+      {
+        text: "Postgres",
+        color: "violet-800",
+        bgColor: "violet-100",
+      },
+    ],
+    description: <MarketMoneyDescription />,
+  },
 ];
 
 export default function Projects() {
@@ -50,13 +87,30 @@ export default function Projects() {
     window.scrollTo(0, 0);
   }, []);
 
+  const [projectIdx, setProjectIdx] = useState(0);
+
   return (
-    <section id="projects">
-      <div>
-        {projectItems.map((proj) => (
-          <ProjectCardLayout key={proj.id} details={proj} />
-        ))}
+    <div className="container mx-auto px-5 md:py-32 pt-10 pb-20 cursor-auto">
+      <div className="flex flex-wrap justify-between text-black dark:text-white">
+        <ProjectCardLayout key={projectItems[projectIdx].id} details={projectItems[projectIdx]} />
+        <div id="projectsMenu" className="md:m-auto md:align-center">
+          <h2 className="text-4xl pb-5 underline">Projects</h2>
+          <div className="wrapper md:pr-6-">
+            <button
+              onClick={() => setProjectIdx(0)}
+              className="block pb-2 hover:scale-125"
+            >
+              Project Portfolio
+            </button>
+            <button
+              onClick={() => setProjectIdx(1)}
+              className="block pb-2 hover:scale-125"
+            >
+              Market Money
+            </button>
+          </div>
+        </div>
       </div>
-    </section>
+    </div>
   );
 }
